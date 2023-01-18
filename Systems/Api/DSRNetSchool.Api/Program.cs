550f3e317e0a348bcb2880c2459f2acc3337d122
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-01-18 22:23:54 +0300

    Мой первый день работы

--- Systems/Api/DSRNetSchool.Api/Program.cs
+++ Systems/Api/DSRNetSchool.Api/Program.cs
@@ -1,10 +1,28 @@
+using DSRNetSchool.Api.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.AddAppLogger();
+
+// Configure services
+var services = builder.Services;
+
+services.AddHttpContextAccessor();
+services.AddAppCors();
+
+services.AddAppHealthChecks();
+services.AddAppVersioning();
+services.AddAppSwagger();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseAppHealthChecks();
+app.UseAppSwagger();
+//app.UseAppCors(); //пока не сделал...
 
 // Configure the HTTP request pipeline.
 
