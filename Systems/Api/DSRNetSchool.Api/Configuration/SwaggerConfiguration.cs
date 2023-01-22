194a7ac81c79551c00ad28b80ec1ea6651975a51
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-01-22 16:40:10 +0300

    Первая практика

--- Systems/Api/DSRNetSchool.Api/Configuration/SwaggerConfiguration.cs
+++ Systems/Api/DSRNetSchool.Api/Configuration/SwaggerConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿namespace DSRNetSchool.Api.Configuration;
 
-//using DSRNetSchool.Services.Settings;
+using DSRNetSchool.Services.Settings;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
@@ -21,10 +21,10 @@ public static class SwaggerConfiguration
     /// <param name="services">Services collection</param>
     /// <param name="mainSettings"></param>
     /// <param name="swaggerSettings"></param>
-    public static IServiceCollection AddAppSwagger(this IServiceCollection services/*, MainSettings mainSettings, SwaggerSettings swaggerSettings*/)
+    public static IServiceCollection AddAppSwagger(this IServiceCollection services, MainSettings mainSettings, SwaggerSettings swaggerSettings)
     {
-        //if (!swaggerSettings.Enabled)
-        //    return services;
+        if (!swaggerSettings.Enabled)
+            return services;
 
         services
             .AddOptions<SwaggerGenOptions>()
@@ -63,14 +63,14 @@ public static class SwaggerConfiguration
                 In = ParameterLocation.Header,
                 Flows = new OpenApiOAuthFlows
                 {
-                    //Password = new OpenApiOAuthFlow
-                    //{
-                    //    TokenUrl = new Uri($"{mainSettings.MainUrl}/connect/token"),
-                    //    Scopes = new Dictionary<string, string>
-                    //    {
-                    //        {"api", "Full API access"},
-                    //    }
-                    //}
+                    Password = new OpenApiOAuthFlow
+                    {
+                        TokenUrl = new Uri($"{mainSettings.MainUrl}/connect/token"),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            {"api", "Full API access"},
+                        }
+                    }
                 }
             });
 
@@ -109,10 +109,10 @@ public static class SwaggerConfiguration
     /// <param name="app">Web application</param>
     public static void UseAppSwagger(this WebApplication app)
     {
-        //var swaggerSettings = app.Services.GetService<SwaggerSettings>();
+        var swaggerSettings = app.Services.GetService<SwaggerSettings>();
 
-        //if (!swaggerSettings?.Enabled ?? false)
-        //    return;
+        if (!swaggerSettings?.Enabled ?? false)
+            return;
 
         var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
--- Systems/Api/DSRNetSchool.Api/Program.cs
+++ Systems/Api/DSRNetSchool.Api/Program.cs
@@ -1,8 +1,14 @@
+using DSRNetSchool.Api;
 using DSRNetSchool.Api.Configuration;
+using DSRNetSchool.Services.Settings;
+using DSRNetSchool.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var mainSettings = Settings.Load<MainSettings>("Main");
+var swaggerSettings = Settings.Load<SwaggerSettings>("Swagger");
 
 builder.AddAppLogger();
 
@@ -14,9 +20,13 @@ services.AddAppCors();
 
 services.AddAppHealthChecks();
 services.AddAppVersioning();
-services.AddAppSwagger();
+services.AddAppSwagger(mainSettings, swaggerSettings);
 
-builder.Services.AddControllers();
+services.AddAppControllerAndViews();
+
+services.RegisterAppServices();
+
+//builder.Services.AddControllers(); //Стандартный, нам не нужен
 
 var app = builder.Build();
 
@@ -26,8 +36,11 @@ app.UseAppSwagger();
 
 // Configure the HTTP request pipeline.
 
-app.UseAuthorization();
+//app.UseAuthorization();
+//app.MapControllers();
 
-app.MapControllers();
+app.UseStaticFiles(); //Сам дописал, чинит вёрстку
+
+app.UseAppControllerAndViews();
 
 app.Run();
