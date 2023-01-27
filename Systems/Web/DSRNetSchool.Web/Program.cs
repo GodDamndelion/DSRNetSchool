bbd233f7b6efb7e9bcb335a4bf89d12a32fe54c8
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-01-27 18:30:16 +0300

    Добавлена симпатичность Пятая практика

--- Systems/Web/DSRNetSchool.Web/Program.cs
+++ Systems/Web/DSRNetSchool.Web/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using DSRNetSchool.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,10 +9,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 
 await builder.Build().RunAsync();
