6eeb9df0a0213d1b28a8aaebae410128a04e632e
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-01-27 22:53:35 +0300

    Добавлена аутентификация Пятая практика

--- Systems/Web/DSRNetSchool.Web/Program.cs
+++ Systems/Web/DSRNetSchool.Web/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using DSRNetSchool.Web;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -9,11 +10,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 
